8eb6fd027d8174c9216a27ca81ec2ad255fadaff
sp560923 <36686705+sp560923@users.noreply.github.com>
2026-02-15 20:27:43 +0530

    Update crew.py

--- crew.py
+++ crew.py
@@ -43,8 +43,7 @@ class ApiTestingCrew():
     groq_llm = LLM(
         model="groq/llama-3.3-70b-versatile", 
         #api_key=os.getenv("GROQ_API_KEY")
-        api_key=groq_api_key
-       api_key="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"
+        api_key=groq_api_key      
     )
 
     @agent
@@ -104,4 +103,5 @@ class ApiTestingCrew():
             verbose=True
 
         )
+
 
